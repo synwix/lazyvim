a5039b853ba18667c13f468f40aa1da8bd0c1634
Mehmet Alp Karatepe <m.alp.krtp@gmail.com>
2024-03-07 21:46:05 +0300

    oxocarbon workin

--- lua/plugins/oxocarbon.lua
+++ lua/plugins/oxocarbon.lua
@@ -1,7 +1,9 @@
 return {
   "nyoom-engineering/oxocarbon.nvim",
-  -- Add in any other configuration;
-  --   event = foo,
-  --   config = bar
-  --   end,
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "oxocarbon",
+    },
+  },
 }
